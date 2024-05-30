32beca03370834ff6180933a6b3d7bfc204aec09
Nelman25 <jonelvillaver735@gmail.com>
2024-05-30 17:47:55 +0800

    updated: changed ui theme

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,7 +1,6 @@
-import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import classes from "../style/loginStyle.module.css";
+import a from "../style/loginStyle.module.css";
 
 export default function LoginPage() {
   const navigate = useNavigate();
@@ -15,51 +14,24 @@ export default function LoginPage() {
   }
   return (
     <>
-      <div className="flex justify-center items-center h-screen flex-col">
-        <h1 className="text-7xl font-bold py-4 drop-shadow-2xl mt-24">
-          CollabSphere
-        </h1>
-        <p className="text-center text-gray-700 text-2xl mb-8">
-          Navigate the workflow seas with ease!
-        </p>
-        <form onSubmit={handleLogin}>
-          <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0 mb-[8rem]">
-            <div className="w-full bg-white rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
-              <div className="w-[25rem] h-[30rem] px-16 py-8  space-y-4 md:space-y-6 sm:p-8 ">
-                <p className="text-xl mt-8 text-align-center font-bold leading-tight tracking-tight text-gray-900 md:text-3xl">
-                  Welcome back!
-                </p>
-                <div>
-                  <label className="block mb-2 text-lg font-medium text-gray-900">
-                    Your username
-                  </label>
-                  <input
-                    placeholder="Username"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                    id="username"
-                    type="text"
-                  />
+      <div class={a.container}>
+        <h1 class={a.title}>CollabSphere</h1>
+        <p class={a.subTitle}>Navigate the workflow seas with ease!</p>
+        <form class={a.formContainer} onSubmit={handleLogin}>
+          <div>
+            <div>
+              <div class={a.formContentContainer}>
+                <p class={a.welcome}>Welcome back!</p>
+                <div class={a.usernameDiv}>
+                  <label>Your username</label>
+                  <input placeholder="Username" id="username" type="text" />
                 </div>
-                <div>
-                  <label className="block mb-2 text-lg font-medium text-gray-900">
-                    Password
-                  </label>
-                  <input
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                    placeholder="••••••••"
-                    id="password"
-                    type="password"
-                  />
+                <div class={a.passwordDiv}>
+                  <label>Password</label>
+                  <input placeholder="••••••••" id="password" type="password" />
                 </div>
-                <button
-                  class="w-full bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  focus:ring-blue-800 text-white"
-                  type="submit"
-                  onClick={handleNavigate}
-                >
-                  Login
-                </button>
 
-                <p className="text-center ">
+                <p class={a.noAccount}>
                   Don't have an account? <Link to="/signup">Sign up</Link>
                 </p>
               </div>
--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -1,73 +1,44 @@
-import React, { useContext } from "react";
-import { LoginContext } from "./LoginContext";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import a from "../style/signUpStyle.module.css";
 
-export default function SignUpPage({ onLogin }) {
+export default function LoginPage() {
   const navigate = useNavigate();
-
-  const { setIsLoggedIn } = useContext(LoginContext);
 
   const handleLogin = (event) => {
     event.preventDefault();
-    onLogin(true);
   };
 
   function handleNavigate() {
     navigate("/dashboard");
   }
   return (
-    <div className="flex justify-center items-center h-screen flex-col">
-      <h1 className="text-6xl py-2 text-center font-bold leading-tight tracking-tight text-gray-900 md:text-6xl">
-        CollabSphere
-      </h1>
-      <p className="text-center text-gray-700 text-xl mb-8">
-        Navigate the workflow seas with ease!
-      </p>
-      <form onSubmit={handleLogin}>
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0 mb-[8rem]">
-          <div class="w-full bg-white rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
-            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <p class="text-xl text-align-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                Create an account
-              </p>
-              <div>
-                <label class="block mb-2 text-sm font-medium text-gray-900">
-                  Enter your username
-                </label>
-                <input
-                  placeholder="Username"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                  id="username"
-                  type="text"
-                />
-              </div>
-              <div>
-                <label class="block mb-2 text-sm font-medium text-gray-900">
-                  Set password
-                </label>
-                <input
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                  placeholder="••••••••"
-                  id="password"
-                  type="password"
-                />
-              </div>
-              <button
-                class="w-full bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  focus:ring-blue-800 text-white"
-                type="submit"
-                onClick={handleNavigate}
-              >
-                Create account
-              </button>
+    <>
+      <div class={a.container}>
+        <h1 class={a.title}>CollabSphere</h1>
+        <p class={a.subTitle}>Navigate the workflow seas with ease!</p>
+        <form class={a.formContainer} onSubmit={handleLogin}>
+          <div>
+            <div>
+              <div class={a.formContentContainer}>
+                <p class={a.welcome}>Welcome!</p>
+                <div class={a.usernameDiv}>
+                  <label>Enter your username</label>
+                  <input placeholder="Username" id="username" type="text" />
+                </div>
+                <div class={a.passwordDiv}>
+                  <label>Set password</label>
+                  <input placeholder="••••••••" id="password" type="password" />
+                </div>
 
-              <p>
-                Already have an account? <Link to="/">Sign in</Link>
-              </p>
+                <p class={a.noAccount}>
+                  Already have an account? <Link to="/signup">Sign in</Link>
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   );
 }
