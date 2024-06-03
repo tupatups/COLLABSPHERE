3b3b878b482bce380bd32918cfbd5888f7b9322c
tupatups <navalchristopherbago@gmail.com>
2024-06-03 11:46:48 +0800

    added signup feature

--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -1,17 +1,37 @@
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import style from "../style/signUpStyle.module.css";
+import { useState } from "react";
+import app from "../firebase"
+import {getAuth, createUserWithEmailAndPassword} from "firebase/auth"
+
+const auth = getAuth(app)
+
 
 export default function LoginPage() {
   const navigate = useNavigate();
 
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   const handleLogin = (event) => {
     event.preventDefault();
-  };
 
-  function handleNavigate() {
-    navigate("/dashboard");
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential
+        console.log(user)
+        navigate("/dashboard");
+      }) 
+  };
+  function handleEmailChange(event){
+    setEmail(event.target.value)
   }
+
+  function handlePasswordChange(event){
+    setPassword(event.target.value)
+  }
+  
   return (
     <>
       <div class={style.container}>
@@ -24,13 +44,13 @@ export default function LoginPage() {
                 <p class={style.welcome}>Welcome!</p>
                 <div class={style.usernameDiv}>
                   <label>Enter your username</label>
-                  <input placeholder="Username" id="username" type="text" />
+                  <input placeholder="Username" id="username" type="text" onChange={handleEmailChange}/>
                 </div>
                 <div class={style.passwordDiv}>
                   <label>Set password</label>
-                  <input placeholder="••••••••" id="password" type="password" />
+                  <input placeholder="••••••••" id="password" type="password" onChange={handlePasswordChange}/>
                 </div>
-                <button class={style.loginSignUpButton} onClick={handleNavigate}>Create Account</button>
+                <button class={style.loginSignUpButton} onClick={handleLogin}>Create Account</button>
                 <p class={style.noAccount}>
                   Already have an account? <Link to="/">Sign in</Link>
                 </p>
